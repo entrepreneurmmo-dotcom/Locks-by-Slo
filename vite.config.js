8fd5b50a2050aae5943f1394039fd88c79328c8d
entrepreneurmmo-dotcom <entrepreneurmmo@gmail.com>
2026-07-06 20:10:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.jsx', '.js', '.tsx', '.ts']
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
